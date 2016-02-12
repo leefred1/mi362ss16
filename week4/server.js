cbfa123b5949ab6e5a1665f8ce32a85ea7e761ff
Frederick Lee <leefred1@msu.edu>
2016-02-12 16:58:28 -0500

    week4

--- week4/server.js
+++ week4/server.js
@@ -2,7 +2,7 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'Content-Type': 'text/html'});
-  response.end('Hello World\n This is a node app!');
+  response.end('Hello World\n');
 }).listen(8124);
 
 console.log('Server running at http://127.0.0.1:8124/');
