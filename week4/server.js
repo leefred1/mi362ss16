a0ff8491c67b1bad725dbb4bdd2b2e8f45d4cae6
Frederick Lee <leefred1@msu.edu>
2016-02-12 17:35:46 -0500

    week4

--- week4/server.js
+++ week4/server.js
@@ -2,8 +2,8 @@ const http = require('http');
 
 http.createServer( (request, response) => {
   response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write(<h1>Hello World</h1>\n);
-  response.write(<p1>This is a node app<p>\n);
+  response.write(<h1>'Hello World\n'</h1>);
+  response.write(<p1>'This is a node app'<p>);
   response.end();
 }).listen(8124);
 
